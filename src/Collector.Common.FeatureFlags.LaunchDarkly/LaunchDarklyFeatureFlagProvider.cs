a34a924cb319eb55e3e5d005cb828dda69f7069c
Adam Lith <adam.lith@gmail.com>
2016-09-16 12:58:22 +0200

    Created solution and LaunchDarkly implementation of IFeatureFlagProvider. CR: Anders Arpi

---
+++ src/Collector.Common.FeatureFlags.LaunchDarkly/LaunchDarklyFeatureFlagProvider.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LaunchDarklyFeatureFlagProvider.cs" company="Collector AB">
+//   Copyright © Collector AB. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Collector.Common.FeatureFlags.LaunchDarkly
+{
+    using global::LaunchDarkly.Client;
+
+    public class LaunchDarklyFeatureFlagProvider : IFeatureFlagProvider
+    {
+        private readonly ILdClient _ldClient;
+        private readonly User _defaultUser;
+
+        public LaunchDarklyFeatureFlagProvider(Configuration configuration)
+        {
+            _ldClient = new LdClient(configuration);
+            _defaultUser = new User("Default");
+        }
+
+        public bool IsFeatureEnabled<T>(bool defaultValue = false) where T : FeatureFlag<bool>, new()
+        {
+            return _ldClient.BoolVariation(new T().Keyname, _defaultUser, defaultValue);
+        }
+
+        public bool GetBoolFlag<T>(bool defaultValue = false) where T : FeatureFlag<bool>, new()
+        {
+            return _ldClient.BoolVariation(new T().Keyname, _defaultUser, defaultValue);
+        }
+
+        public int GetIntFlag<T>(int defaultValue) where T : FeatureFlag<int>, new()
+        {
+            return _ldClient.IntVariation(new T().Keyname, _defaultUser, defaultValue);
+        }
+
+        public string GetStringFlag<T>(string defaultValue) where T : FeatureFlag<string>, new()
+        {
+            return _ldClient.StringVariation(new T().Keyname, _defaultUser, defaultValue);
+        }
+    }
+}
