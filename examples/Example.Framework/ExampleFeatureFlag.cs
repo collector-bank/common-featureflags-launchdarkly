8438a9feb20ab92d33cf6a1eccc03d988a5744bd
Andreas Andersson <Andreas.Andersson@collectorbank.se>
2018-10-26 14:59:02 +0200

    Updated LaunchDarkly.Client to Version 5.4.0
    Fixed dependency mess that caused net452 to depend on netstandard 2.0 implicitly
    
    Split examples into NetCore and Fraemwork test applications
    
    Used #ifdef and netcoreapp2.0 target for unit test. Also switched from XUnit to NUnit 3

---
+++ examples/Example.Framework/ExampleFeatureFlag.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExampleFeatureFlag.cs" company="Collector AB">
+//   Copyright © Collector AB. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Example.Framework
+{
+    using Collector.Common.FeatureFlags;
+
+    public class ExampleFeatureFlag : FeatureFlag<bool>
+    {
+        public override string Keyname => "my-feature-flag-key";
+    }
+}
---
+++ examples/Example.Framework/Program.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Collector AB">
+//   Copyright © Collector AB. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Example.Framework
+{
+    using System;
+
+    using Collector.Common.FeatureFlags;
+    using Collector.Common.FeatureFlags.LaunchDarkly;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var provider = LaunchDarklyFeatureFlagProviderBuilder.CreateFromAppSettings().Build();
+           
+            var user = new User("my-team-name", "my-user-key")
+                          .With("my-custom-attribute", "my-custom-value");
+
+            Console.WriteLine("Press enter to check feature status, 'q' to quit\n");
+
+            var keepRunning = true;
+            while (keepRunning)
+            {
+                var featureEnabledForDefaultUser = provider.IsFeatureEnabled<ExampleFeatureFlag>();
+                Console.WriteLine($"Feature enabled for default user: {featureEnabledForDefaultUser}");
+
+                var featureEnabledForSpecificUser = provider.IsFeatureEnabled<ExampleFeatureFlag>(user);
+                Console.WriteLine($"Feature enabled for specific user: {featureEnabledForSpecificUser}");
+
+                Console.Write(">");
+                var input = Console.ReadLine();
+
+                if (input == null || input.ToLower() == "q")
+                    keepRunning = false;
+            }
+        }
+    }
+}
